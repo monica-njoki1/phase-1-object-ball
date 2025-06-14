a9b78562a114f45a8a21b2e62c548abc8ef136fa
monica <monica.njoki@student.moringaschool.com>
2025-06-15 01:28:56 +0300

    object ball

---
+++ test/index.js
@@ -0,0 +1,22 @@
+function numPointsScored (players) {
+    return players[points]
+}
+const game= {
+        teamName:"Freenet",
+        colors: ["black", "white"],
+        players: {
+            "Alan Anderson": {
+                number: 0,
+                shoe: 16,
+                points: 22,
+                rebounds: 12,
+                assists: 12,
+                steals: 3,
+                blocks: 1,
+                slamDunks: 1
+
+            }
+        }
+    }
+
+  
